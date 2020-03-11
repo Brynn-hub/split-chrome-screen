85327d12e1b47f5efa7d065a7f52b5a5b0a0da36
br126 <wanglubr@126.com>
2020-03-11 22:48:15 +0800

    feature/add-command

--- README.md
+++ README.md
@@ -6,4 +6,7 @@
 点击 [背景页](chrome://extensions/) 可以debugger background.js
 
 1. 我们实现一个分屏插件，需要操作tab， 所以先搜索[tab](https://developer.chrome.com/extensions/tabs)
-1. 分屏需要让页面弹出来，要操作windows，搜索[window](https://developer.chrome.com/extensions/windows)
+2. 分屏需要让页面弹出来，要操作windows，搜索[window](https://developer.chrome.com/extensions/windows) 
+[code](https://github.com/Brynn-hub/split-chrome-screen/tree/feature/init-extension)
+
+3. 当操作快捷键的时候，使页面弹出来，搜索[command](https://developer.chrome.com/extensions/commands)
--- background.js
+++ background.js
@@ -1,9 +1,36 @@
-chrome.tabs.query({
-  active: true,
-  currentWindow: true
-}, (tabs) => {
-  chrome.windows.create({
-    tabId: tabs[0].id
+chrome.commands.onCommand.addListener(command => {
+  let options = {};
+
+  switch (command) {
+    case 'split-screen-left':
+      options = {
+        left: 0,
+        width: window.screen.availWidth / 2
+      }
+      break;
+    case 'split-screen-right':
+      options = {
+        left: window.screen.availWidth / 2,
+        width: window.screen.availWidth / 2
+      }
+      break;
+    default:
+      options = {
+        left: 0,
+        width: window.screen.availWidth
+      }
+      break;
+  }
+
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, (tabs) => {
+
+    chrome.windows.create({
+      tabId: tabs[0].id,
+      ...options
+    })
   })
 })
 
