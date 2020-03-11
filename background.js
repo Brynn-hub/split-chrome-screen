b244fbf2adbd18f9e978bee95039203ad20d4873
br126 <wanglubr@126.com>
2020-03-11 22:57:16 +0800

    feature/optimize-switch

--- background.js
+++ background.js
@@ -1,35 +1,27 @@
+const fullWidth = window.screen.availWidth;
+const commands = {
+  'split-screen-left': {
+    left: 0,
+    width: fullWidth / 2
+  },
+  'split-screen-right': {
+    left: fullWidth / 2,
+    width: fullWidth / 2
+  },
+  'split-screen-full': {
+    left: 0,
+    width: fullWidth
+  },
+};
+
 chrome.commands.onCommand.addListener(command => {
-  let options = {};
-
-  switch (command) {
-    case 'split-screen-left':
-      options = {
-        left: 0,
-        width: window.screen.availWidth / 2
-      }
-      break;
-    case 'split-screen-right':
-      options = {
-        left: window.screen.availWidth / 2,
-        width: window.screen.availWidth / 2
-      }
-      break;
-    default:
-      options = {
-        left: 0,
-        width: window.screen.availWidth
-      }
-      break;
-  }
-
   chrome.tabs.query({
     active: true,
     currentWindow: true
   }, (tabs) => {
-
     chrome.windows.create({
       tabId: tabs[0].id,
-      ...options
+      ...commands[command]
     })
   })
 })
